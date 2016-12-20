00f91f0e82c830af17a1a818d11e84bfb86ceb81
Ilya Bohaslauchyk <3751950@gmail.com>
2016-12-20 14:31:23 +0000

    Auth with saga

--- app/containers/Auth/actions.js
+++ app/containers/Auth/actions.js
@@ -13,17 +13,9 @@ var router = require('react-router');
 export function defaultAction(user) {
   //router.browserHistory.push('/');
   
-  var request = new Promise ((resolve, reject) => {
-    //Here should be an authentication request to server
-    setTimeout(() => {
-      if (user.username === "admin" && user.password === "admin") resolve(user);
-      else reject(new Error("Wrong credentials"));
-    }, 0);
-  });
-  
   return {
     type: AUTH_ACTION,
-    payload: request
+    payload: user
   };
 
 }
--- app/containers/Auth/sagas.js
+++ app/containers/Auth/sagas.js
@@ -1,11 +1,37 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
+ 
+import {
+  AUTH_ACTION
+} from './constants';
 
 // Individual exports for testing
 export function* defaultSaga() {
-  return;
+  yield takeEvery(AUTH_ACTION, fetchUser);
+}
+
+function* fetchUser (action) {
+  try {
+    const user = yield call(fetch, action.payload);
+    console.log("success");
+    yield put({type:AUTH_ACTION, user: user});
+  } catch (err) {
+    console.log("error");
+    yield put({type:AUTH_ACTION, user: err});
+  }
 }
 
 // All sagas to be loaded
 export default [
   defaultSaga,
 ];
+
+function fetch (user) {
+  var request = new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      if (user.username === "admin" && user.password === "admin") resolve(user);
+      else reject(new Error("Wrong credentials"));
+    }, 1000);
+  });
+  
+  return request;
+};
