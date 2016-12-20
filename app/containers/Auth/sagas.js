5e688500710c190c172a867127a8d23065ce1365
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-12-20 19:54:48 +0300

    Auth is a start page

--- app/containers/Auth/sagas.js
+++ app/containers/Auth/sagas.js
@@ -2,6 +2,8 @@ import {  takeEvery } from 'redux-saga';
 import {  take, call, put, select } from 'redux-saga/effects';
 
 import fetch from "./fetch";
+
+var router = require('react-router');
  
 import {
   AUTH_ACTION,
@@ -17,6 +19,7 @@ export function* defaultSaga() {
 function* fetchUser (action) {
   try {
     const user = yield call(fetch, action.payload);
+    router.browserHistory.push('/home');
     yield put({type: AUTH_SUCCESS, payload: user});
   } catch (err) {
     yield put({type: AUTH_DENIED, payload: err});
