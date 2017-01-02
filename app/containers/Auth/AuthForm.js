f6b3910b0100a234a55ccc7edca0444de6ad0c8c
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-01-02 19:37:19 +0300

    Field

--- app/containers/Auth/AuthForm.js
+++ app/containers/Auth/AuthForm.js
@@ -2,37 +2,43 @@ import React from "react";
 import { Field, reduxForm } from "redux-form/immutable";
 import styled from "styled-components";
 
-const Input = styled.input`
-    background-color: #fff;
-`;
-
 const Form = styled.form`
     background-color: #0050a9;
     padding: 15px;
     border-radius: 10px;
 `;
 
+const renderField = ({ input, label, placeholder, type, meta: { touched, error } }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <div>
+        <input {...input} type={type} placeholder={placeholder} className="form-control"/>
+        {touched && error && <span>{error}</span>}
+      </div>
+    </div>
+  );
+}
+
 const AuthForm = (props) => {
   return (
     <Form onSubmit={props.handleSubmit}>
       <div className="form-group">
-        <label htmlFor="name">Name</label>
         <Field
           name="name"
           type="text"
           placeholder="Enter your name"
-          className="form-control"
-          component="Input"
+          component={renderField}
+          label="Name"
         />
       </div>
       <div className="form-group">
-        <label htmlFor="password">Password</label>
         <Field
           name="password"
           type="password"
           placeholder="Enter your password"
-          className="form-control"
-          component="Input"
+          component={renderField}
+          label="Password"
         />
       </div>
       <input type="submit" value="Send" className="btn btn-default"/>
@@ -40,6 +46,15 @@ const AuthForm = (props) => {
   )
 };
 
+/*const validate = (values) => {
+  let errors = {};
+
+  if (!values.name) errors.name = "Required";
+
+  return errors;
+};*/
+
 export default reduxForm ({
-  form: "authForm"
+  form: "authForm",
+  //validate
 })(AuthForm);
